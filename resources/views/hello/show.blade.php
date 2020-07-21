0374b9671be68c564544ecf684ab99dc9f509a7a
hirokichi1992 <plastar14105@gmail.com>
2020-07-22 07:33:34 +0900

    演算機能を利用した検索方法

--- resources/views/hello/show.blade.php
+++ resources/views/hello/show.blade.php
@@ -8,24 +8,20 @@
 @endsection
 
 @section('content')
-<table>
+@if($items != null)
+@foreach($items as $item)
+<table width="400px">
     <tr>
-        <th>id: </th>
-        <td>{{$item->id}}</td>
+        <th width="50%">id: </th>
+        <td width="50%">{{$item->id}}</td>
     </tr>
     <tr>
-        <th>name: </th>
-        <td>{{$item->name}}</td>
-    </tr>
-    <tr>
-        <th>mail: </th>
-        <td>{{$item->mail}}</td>
-    </tr>
-    <tr>
-        <th>age: </th>
-        <td>{{$item->age}}</td>
+        <th width="50%">name: </th>
+        <td width="50%">{{$item->name}}</td>
     </tr>
 </table>
+@endforeach
+@endif
 @endsection
 
 @section('footer')
