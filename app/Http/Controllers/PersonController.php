31daf23a969d5e77b7ff7890a9a2f32117331ad4
hirokichi1992 <plastar14105@gmail.com>
2020-07-23 07:59:22 +0900

    where()メソッドを使用したname検索方法

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -23,7 +23,7 @@ class PersonController extends Controller
     // ID検索アクション
     public function search(Request $request)
     {
-        $item = Person::find($request->input);
+        $item = Person::where('name', $request->input)->first();
         $param = [
             'input' => $request->input,
             'item' => $item,
