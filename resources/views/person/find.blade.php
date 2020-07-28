fc868aeed9c0d84c89c6b5a5a84a28f6b73d5965
hirokichi1992 <plastar14105@gmail.com>
2020-07-29 08:31:03 +0900

    Personページに検索ページへの遷移を追加・検索ページのデザインにBootstrapを適用

--- resources/views/person/find.blade.php
+++ resources/views/person/find.blade.php
@@ -8,10 +8,15 @@
 @endsection
 
 @section('content')
+<button type="button" onclick="history.back()" class="btn btn-primary" style="margin-bottom: 1rem;">Back</button>
 <form action="/person/find" method="post">
     @csrf
-    <input type="text" name="input" value="{{$input}}">
-    <input type="submit" value="IDを検索する">
+    <div class="input-group mb-3">
+        <input type="text" name="input" value="{{$input}}" class="form-control" placeholder="Enter age" aria-label="Recipient's username" aria-describedby="button-addon2">
+        <div class="input-group-append">
+            <button class="btn btn-outline-secondary" type="submit" id="button-addon2">Search</button>
+        </div>
+    </div>
 </form>
 @if(isset($item))
 <table>
