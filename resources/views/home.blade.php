0de6889b0010e65bc7b67df7fede22e265fd3079
hirokichi1992 <plastar14105@gmail.com>
2020-07-28 09:45:20 +0900

    ホーム画面から各機能に遷移できるようにホーム画面のレイアウトを変更

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,16 +5,34 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                <div class="card-header" style="text-align: center;">Function List</div>
+                <div class="card" style="width: 18rem; margin: 1rem auto;">
+                    <div class="card-body">
+                        <h5 class="card-title">Hello</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="/hello" class="btn btn-primary">Go to Hello</a>
+                    </div>
+                </div>
+                <div class="card" style="width: 18rem; margin: 1rem auto;">
+                    <div class="card-body">
+                        <h5 class="card-title">Person</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="/person" class="btn btn-primary">Go to Person</a>
+                    </div>
+                </div>
+                <div class="card" style="width: 18rem; margin: 1rem auto;">
+                    <div class="card-body">
+                        <h5 class="card-title">Board</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="/board" class="btn btn-primary">Go to Board</a>
+                    </div>
+                </div>
+                <div class="card" style="width: 18rem; margin: 1rem auto;">
+                    <div class="card-body">
+                        <h5 class="card-title">RESTApp</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="/rest" class="btn btn-primary">Go to RESTApp</a>
+                    </div>
                 </div>
             </div>
         </div>
